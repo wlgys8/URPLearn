e097098aa5900ccd6e873b46307498b49bd64fc3
zilch <wlgys8@gmail.com>
2021-03-06 17:52:47 +0800

    rename vars

--- Assets/URPLearn/Blur/Shader/Blur.hlsl
+++ Assets/URPLearn/Blur/Shader/Blur.hlsl
@@ -1,50 +1,60 @@
 
-
-half4 BoxBlur(Texture2D tex,float2 pixelCoord,float halfSampleCount,float2 offset){
+half4 BoxBlur(Texture2D tex,float2 pixelCoord,float halfKernelSize){
     half4 color = half4(0,0,0,1);
-    float weight = rcp(2 * halfSampleCount + 1);
-    for(int i =  -halfSampleCount ; i <= halfSampleCount ; i ++){
+    int kernelSize = 2 * halfKernelSize + 1;
+    float weight = rcp(kernelSize * kernelSize);
+    for(int i =  -halfKernelSize ; i <= halfKernelSize ; i ++){
+        for(int j =  -halfKernelSize ; j <= halfKernelSize ; j ++){
+            color += LOAD_TEXTURE2D_X(tex,pixelCoord + float2(i,j)) * weight;
+        }
+    }
+    return color ;
+}
+
+half4 BoxBlur(Texture2D tex,float2 pixelCoord,float halfKernelSize,float2 offset){
+    half4 color = half4(0,0,0,1);
+    float weight = rcp(2 * halfKernelSize + 1);
+    for(int i =  -halfKernelSize ; i <= halfKernelSize ; i ++){
         color += LOAD_TEXTURE2D_X(tex,pixelCoord + offset * i) * weight;
     }
     return color ;
 }
 
 ///BoxFilter水平采样
-half4 BoxBlurH(Texture2D tex,float2 pixelCoord,int halfSampleCount,float radiusScale){
-    return BoxBlur(tex,pixelCoord,halfSampleCount,float2(radiusScale,0));
+half4 BoxBlurH(Texture2D tex,float2 pixelCoord,int halfKernelSize,float radiusScale){
+    return BoxBlur(tex,pixelCoord,halfKernelSize,float2(radiusScale,0));
 }
 
 ///BoxFilter垂直采样
-half4 BoxBlurV(Texture2D tex,float2 pixelCoord,int halfSampleCount,float radiusScale){
-    return BoxBlur(tex,pixelCoord,halfSampleCount,float2(0,radiusScale));
+half4 BoxBlurV(Texture2D tex,float2 pixelCoord,int halfKernelSize,float radiusScale){
+    return BoxBlur(tex,pixelCoord,halfKernelSize,float2(0,radiusScale));
 }
 
-half4 BoxBlurBilinear(Texture2D tex,sampler texSampler,float2 uv,int halfSampleCount,float2 offset){
+half4 BoxBlurBilinear(Texture2D tex,sampler linearSampler,float2 uv,int halfKernelSize,float2 offset){
     half4 color = half4(0,0,0,1);
-    float weight = rcp(halfSampleCount * 2 + 1);
-    if(halfSampleCount % 2 == 0){ //even
-        color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv) * weight;
-        int quartSampleCount = floor(halfSampleCount / 2);
-        for(int i = 1; i <= quartSampleCount; i ++){
+    float weight = rcp(halfKernelSize * 2 + 1);
+    if(halfKernelSize % 2 == 0){ //even
+        color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv) * weight;
+        int quartKernelSize = floor(halfKernelSize / 2);
+        for(int i = 1; i <= quartKernelSize; i ++){
             float uvOffset = (i * 2 - 0.5);
-            color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv + offset * uvOffset) * 2 * weight;
-            color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv - offset * uvOffset) * 2 * weight;
+            color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv + offset * uvOffset) * 2 * weight;
+            color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv - offset * uvOffset) * 2 * weight;
         }
     }else{ //odd
-        color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv + 0.75 * offset) * 1.5 * weight;
-        color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv - 0.75 * offset) * 1.5 * weight;
-        int quartSampleCount = floor((halfSampleCount -1) / 2);
-        for(int i = 1; i <= quartSampleCount; i ++){
+        color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv + 0.75 * offset) * 1.5 * weight;
+        color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv - 0.75 * offset) * 1.5 * weight;
+        int quartKernelSize = floor((halfKernelSize -1) / 2);
+        for(int i = 1; i <= quartKernelSize; i ++){
             float uvOffset = (i * 2 + 0.5);
-            color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv + offset * uvOffset) * 2 * weight;
-            color += SAMPLE_TEXTURE2D_X(tex,texSampler,uv - offset * uvOffset) * 2 * weight;
+            color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv + offset * uvOffset) * 2 * weight;
+            color += SAMPLE_TEXTURE2D_X(tex,linearSampler,uv - offset * uvOffset) * 2 * weight;
         }
     }
-
     return color ;
 }
 
-#define BOX_BLUR_BILINEAR(tex,uv,halfSampleCount,offset) BoxBlurBilinear(tex,sampler_LinearClamp,uv,halfSampleCount,offset)
+#define BOX_BLUR_BILINEAR(tex,uv,halfKernelSize,offset) BoxBlurBilinear(tex,sampler_LinearClamp,uv,halfKernelSize,offset)
 
 
 /**
